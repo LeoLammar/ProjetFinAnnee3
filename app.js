329890d2865d6ba2256a2cccaff757ac2ac22bb8
Antonin-D <antonin.dumas@student.junia.com>
2025-05-22 12:24:02 +0200

    M2

--- app.js
+++ app.js
@@ -20,3 +20,28 @@ app.get('/emploidutemps', (req, res) => {
 app.listen(3000, () => {
     console.log('Serveur démarré sur http://localhost:3000');
 });
+
+app.get('/transformations-integrales', (req, res) => {
+    const coursDisponibles = [
+        { name: 'Cours 1 : Introduction aux Transformations Intégrales', link: '/documents/Partiel_HEIencule_2022_Corrige.pdf' },
+        { name: 'Cours 2 : Applications des Transformations Intégrales', link: '/documents/TD_EA4_approximations_des_filtres.pdf' },
+    ];
+    res.render('transformations-integrales', { cours: coursDisponibles });
+});
+
+app.get('/mecanique-du-solide', (req, res) => {
+    res.render('mecanique-du-solide');
+  });
+  
+  app.get('/probabilites-statistiques', (req, res) => {
+    res.render('probabilites-statistiques');
+  });
+  
+  app.get('/ressources-educatives', (req, res) => {
+    res.render('ressources-educatives');
+  });
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:${3000}`);
+  });
